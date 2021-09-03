41892169ccb3741a06ad3a044bd9e07a8ec26d5b
Ubuntu <evaisnotonline@gmail.com>
2021-09-03 08:52:35 +0000

    QDP-7 changing env variables for security

--- backend/application/__init__.py
+++ backend/application/__init__.py
@@ -5,7 +5,7 @@ from os import getenv
 app = Flask(__name__)
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:XeQkrjLNwQ6qySn@projectdb.cjmj7j0zzqco.eu-west-1.rds.amazonaws.com:3306/users'
+app.config['SQLALCHEMY_DATABASE_URI'] = str(getenv('DATABASE_URI'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = str(getenv('SECRET_KEY'))
 db = SQLAlchemy(app)
